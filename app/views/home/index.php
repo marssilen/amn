d2e8e4b41e388c0b4533383cbe6ca92844eed8b1
marssilen <missa@chmail.ir>
2017-10-13 22:33:05 +0330

    m

--- app/views/home/index.php
+++ app/views/home/index.php
@@ -40,7 +40,7 @@ require_once('app/views/head.php'); ?>
 </div>
 <!-- Promo Section - "We know design" -->
 <?php foreach($data['div'] as $div){?>
-<div class="w3-container w3-grey" style="padding:128px 16px">
+<div class="w3-container w3-dark-grey" style="padding:128px 0px">
     <div class="w3-row-padding">
         <div class="w3-col m6">
             <h3><?=$div['title']?></h3>
@@ -48,7 +48,7 @@ require_once('app/views/head.php'); ?>
             <p><a href="<?=URL?>page/<?=$div['url_cat'].'/'.urlencode($div['title'])?>" class="w3-button w3-black">ادامه</a></p>
         </div>
         <div class="w3-col m6">
-            <img class="w3-image w3-round-large" src="<?= URL ?>public/upload/<?=$div['image']?>" alt="AMNCO" width="700" height="394">
+            <img class="w3-image" src="<?= URL ?>public/upload/<?=$div['image']?>" alt="AMNCO" width="100%">
         </div>
     </div>
 </div>
