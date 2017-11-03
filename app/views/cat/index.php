95c5a0c4e71d185455e0524f65bfde9aa4a95c16
marssilen <missa@chmail.ir>
2017-11-03 22:38:21 +0330

    x

--- app/views/cat/index.php
+++ app/views/cat/index.php
@@ -3,13 +3,35 @@ $title=$data['cat'];
 $desc="دسته بندی عکس ".$data['cat'];;
 require_once('app/views/head.php'); ?>
 <body>
+<div id="wrapper">
 <?php require_once('app/views/menu.php'); ?>
-<div class="w3-row container" style="margin-top: 70px">
+<?php 
+//require_once('app/views/header.php'); 
+?>
+<div id="content" style="margin-top: 100px">
+<div class="w3-row container w3-white w3-card-2" style="padding: 10px;margin-top: 10px;margin-bottom: 10px">
+    <div>
+        <p>
+            <a class="grey" href="">فروشگاه اینترنتی امن ایران</a>
+            <a class="grey" href=""><?=$data['cat']?></a>
+            <a class="grey" href="">...</a>
+        </p>
+        <p>
+            <span style="font-size: large"><?=$data['cat']?></span>
+            <span class="w3-text-gray">(نمایش 1-40 از 400)</span>
+        </p>
+        <p class="sort">
+            مرتب سازی بر اساس:
+            <a href="">پربازدید ترین ها</a>
+            <a href="">جدیدترین ها</a>
+        </p>
+        <hr>
+    </div>
     <?php foreach ($data['items'] as $item) {?>
         <div class="w3-col m3 s6" style="padding: 5px">
-            <div class="img_c w3-card-2 w3-hover-shadow w3-round">
-                <a href="<?=URL?>page/<?=$item['id'].'/'.urlencode($item['name'])?>">
-                <img src="<?= URL ?>public/upload/<?=$item['card_image']?>" alt="MIM PHOTOGRAPHY" style="width: 100%">
+            <div class="img_c w3-border w3-hover-border-blue">
+                <a href="<?=URL?>page/<?=$item['id'].'/'.$item['name']?>">
+                <img src="<?= URL ?>public/upload/<?=$item['card_image']?>" alt="AMN IRAN SHOP" style="width: 100%">
                 </a>
                 <p class="w3-center" style="margin-top: 10px;">
                     &ensp;<?=$item['name']?>
@@ -24,8 +46,10 @@ require_once('app/views/head.php'); ?>
   <?=$data['pview']?>
 </div>
 </div>
+</div>
 <?php
 require_once ('app/views/footer.php');
 ?>
+</div>
 </body>
 </html>
